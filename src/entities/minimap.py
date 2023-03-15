62a1b51154b3b67b52759400c96b4d1c10de4ad2
Abdullah Akcam <abdullah.akcam52@gmail.com>
2023-03-15 21:41:40 +0300

    general improvements

--- src/entities/minimap.py
+++ src/entities/minimap.py
@@ -1,60 +1,60 @@
-import time
-from random import randint
-import pyautogui
-
-from windowcapture import WindowCapture
-
-
-class MiniMap:
-    # Map offsite from right: 212
-    # Map offsite from top: 249
-
-    # coordinates of top left corner of minimap
-    OFFSET_X_FROM_RIGHT = 204
-    OFFSET_Y_FROM_TOP = 116
-
-    # minimap size
-    WIDTH = 135
-    HEIGHT = 135
-
-    win_cap: WindowCapture = None
-    detector = None
-
-    def __init__(self, win_cap, detector):
-        self.win_cap = win_cap
-        self.detector = detector
-
-        self.x = win_cap.w - self.OFFSET_X_FROM_RIGHT
-        self.y = self.OFFSET_Y_FROM_TOP
-
-    def click_random_position(self):
-        print("Clicking random position on minimap")
-        # click a random position on the screen
-        x = randint(0, self.WIDTH)
-        y = randint(0, self.HEIGHT)
-        x, y = self.get_screen_position((x, y))
-        pyautogui.moveTo(x=x, y=y)
-        time.sleep(1.250)
-        pyautogui.click()
-
-    def get_area(self, img):
-        return img[
-               self.OFFSET_Y_FROM_TOP: self.OFFSET_Y_FROM_TOP + self.HEIGHT,
-               self.win_cap.w - self.OFFSET_X_FROM_RIGHT: self.win_cap.w - self.OFFSET_X_FROM_RIGHT + self.WIDTH,
-               ]
-
-    def click_next_target(self):
-        screen_x, screen_y = self.get_screen_position(self.detector.minimap_targets[0])
-        print("Minimap: Moving mouse to:{} y:{}".format(screen_x, screen_y))
-        pyautogui.moveTo(x=screen_x, y=screen_y)
-
-        # short pause to let the mouse movement complete and allow
-        time.sleep(1.250)
-
-        print("Minimap: Click on target at x:{} y:{}".format(screen_x, screen_y))
-        pyautogui.click()
-
-    def get_screen_position(self, pos):
-        return \
-            pos[0] + self.win_cap.offset_x + self.win_cap.w - self.OFFSET_X_FROM_RIGHT, \
-            pos[1] + self.win_cap.offset_y + self.OFFSET_Y_FROM_TOP
+# import time
+# from random import randint
+# import pyautogui
+#
+# from windowcapture import WindowCapture
+#
+#
+# class MiniMap:
+#     # Map offsite from right: 212
+#     # Map offsite from top: 249
+#
+#     # coordinates of top left corner of minimap
+#     OFFSET_X_FROM_RIGHT = 204
+#     OFFSET_Y_FROM_TOP = 116
+#
+#     # minimap size
+#     WIDTH = 135
+#     HEIGHT = 135
+#
+#     win_cap: WindowCapture = None
+#     detector = None
+#
+#     def __init__(self, win_cap, detector):
+#         self.win_cap = win_cap
+#         self.detector = detector
+#
+#         self.x = win_cap.w - self.OFFSET_X_FROM_RIGHT
+#         self.y = self.OFFSET_Y_FROM_TOP
+#
+#     def click_random_position(self):
+#         print("Clicking random position on minimap")
+#         # click a random position on the screen
+#         x = randint(0, self.WIDTH)
+#         y = randint(0, self.HEIGHT)
+#         x, y = self.get_screen_position((x, y))
+#         pyautogui.moveTo(x=x, y=y)
+#         time.sleep(1.250)
+#         pyautogui.click()
+#
+#     def get_area(self, img):
+#         return img[
+#                self.OFFSET_Y_FROM_TOP: self.OFFSET_Y_FROM_TOP + self.HEIGHT,
+#                self.win_cap.w - self.OFFSET_X_FROM_RIGHT: self.win_cap.w - self.OFFSET_X_FROM_RIGHT + self.WIDTH,
+#                ]
+#
+#     def click_next_target(self):
+#         screen_x, screen_y = self.get_screen_position(self.detector.minimap_targets[0])
+#         print("Minimap: Moving mouse to:{} y:{}".format(screen_x, screen_y))
+#         pyautogui.moveTo(x=screen_x, y=screen_y)
+#
+#         # short pause to let the mouse movement complete and allow
+#         time.sleep(1.250)
+#
+#         print("Minimap: Click on target at x:{} y:{}".format(screen_x, screen_y))
+#         pyautogui.click()
+#
+#     def get_screen_position(self, pos):
+#         return \
+#             pos[0] + self.win_cap.offset_x + self.win_cap.w - self.OFFSET_X_FROM_RIGHT, \
+#             pos[1] + self.win_cap.offset_y + self.OFFSET_Y_FROM_TOP
